ef01cecc74db9ffebd6031d0ecdfc705f4aafe2c
Alex Lyan <alexlyan1612@gmail.com>
2022-03-21 02:45:44 +0300

    added error scenario

--- request_rate/daily_rates.py
+++ request_rate/daily_rates.py
@@ -29,10 +29,15 @@ def get_usd_kgs_last_15mins() -> str:
     """
     buy_data = requests.get(
         "https://valuta.kg/api/rates.json?currency=usd"
-    ).json()["data"]["buy"]
-    buy_data = pd.DataFrame(data=buy_data, columns=["date", "buy_value"])
-    buy_data["date"] = buy_data["date"].apply(convert_unix_to_normal)
+    ).json()
+    if not buy_data["error"]:
+        buy_data = buy_data["data"]["buy"]
+        buy_data = pd.DataFrame(data=buy_data, columns=["date", "buy_value"])
+        buy_data["date"] = buy_data["date"].apply(convert_unix_to_normal)
 
-    avg_value = buy_data.sort_values("date").tail(2)["buy_value"].mean()
+        avg_value = buy_data.sort_values("date").tail(2)["buy_value"].mean()
 
-    return avg_value
+        return avg_value
+
+    else:
+        return "Values are not valid this time, use avg 1d method"
