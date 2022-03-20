86f69c7c6bbb2047c183a3d43c36d8e20989fec7
Alex Lyan <alexlyan1612@gmail.com>
2022-03-20 10:52:57 +0300

    modified project root dirs

--- main.py
+++ main.py
@@ -3,28 +3,8 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-import requests
-from datetime import datetime
-import pandas as pd
-import os
-
-
-def convert_unix_to_normal(ts):
-    ts = ts//1000
-    ts = int(ts)
-
-    return datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-
-
-def get_usd_kgs_1d():
-    buy_data = requests.get("https://valuta.kg/api/rates/history.json?currency=usd").json()["data"]["buy"]
-    buy_data = pd.DataFrame(data=buy_data, columns=["date", "buy_value"])
-    buy_data["date"] = buy_data["date"].apply(convert_unix_to_normal)
-
-    avg_value = buy_data.sort_values("date").tail(1)["buy_value"].values[0]
-
-    return avg_value
+from telegram.ext import CallbackContext, CommandHandler, Updater
+from request_rate.daily_rates import get_usd_kgs_1d
 
 
 def get_cur_1d(update: Update, context: CallbackContext) -> None:
