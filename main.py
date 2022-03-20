2e2e404c4f21e31d94373a1e3263f9a9cfa3cbdc
Alex Lyan <alexlyan1612@gmail.com>
2022-03-20 14:05:41 +0300

    add values for last 15mins

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, Updater
 
-from request_rate.daily_rates import get_usd_kgs_1d
+from request_rate.daily_rates import get_usd_kgs_1d, get_usd_kgs_last_15mins
 
 updater = Updater('5250151891:AAHVazpIZVQoROOeJwqk_WGyPYHd8DN-Gms')
 
@@ -18,7 +18,18 @@ def get_cur_1d(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Avg. Currency for today: {get_usd_kgs_1d()}')
 
 
+def get_cur_15min(update: Update, context: CallbackContext) -> None:
+    """Send telegram message with value.
+
+    :param update:
+    :param context:
+    :return:
+    """
+    update.message.reply_text(f'Avg. Currency for last 15 mins: {get_usd_kgs_last_15mins()}')
+
+
 updater.dispatcher.add_handler(CommandHandler('cur1d', get_cur_1d))
+updater.dispatcher.add_handler(CommandHandler('cur15mins', get_cur_15min))
 
 
 # Press the green button in the gutter to run the script.
